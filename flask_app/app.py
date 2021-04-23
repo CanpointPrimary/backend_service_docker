28c77d5f2d9a7f44af26e194c7078080ddd193ed
gwjrl <g715450338@163.com>
2021-04-23 22:17:01 +0800

    refactor: modify host

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,9 +1,8 @@
 import os
 
-from flask_project import create_app, setting
+from flask_project import create_app
+
 app = create_app(os.getenv('FLASK_ENV', 'default'))
 
-
 if __name__ == '__main__':
-    host_ip = setting.HOST_IP
-    app.run(host=host_ip)
+    app.run(host='')
